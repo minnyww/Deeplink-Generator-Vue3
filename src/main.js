795023830ab40a0cec3792eea0ce39ca43c0af57
minnyww <minrrverse@gmail.com>
2020-09-27 20:53:16 +0700

    [init]

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import Vue, { createApp } from 'vue';
+import App from './App.vue';
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
+Vue.use(Antd);
 
-createApp(App).mount('#app')
+createApp(App).mount('#app');
